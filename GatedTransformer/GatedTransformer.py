7dc84551ffbd608b8fb5854e69a11e9d6648e51b
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-10-30 03:43:08 +0100

    Update GatedTransformer.py

--- GatedTransformer/GatedTransformer.py
+++ GatedTransformer/GatedTransformer.py
@@ -6,7 +6,6 @@ from embedding import EmbeddingLayer
 from encoder import EncoderLayer
 
 
-
 class GatedTransformer(nn.Module):
     def __init__(self, input_dim=45, d_model=512, num_heads=8, d_ff=2048, num_layers=6):
         super(GatedTransformer, self).__init__()
@@ -15,26 +14,21 @@ class GatedTransformer(nn.Module):
         self.encoders = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff) for _ in range(num_layers)])
         self.classifier = nn.Linear(d_model, 5)  # 5 outputs for person, age, height, weight, and gender
 
-        def forward(self, x):
-            print(f"Input shape: {x.shape}")  # Debugging line
-    
-            x = self.embedding(x)
-            print(f"After embedding shape: {x.shape}")  # Debugging line
-    
-            x = x.unsqueeze(0)  # Introduce a sequence length dimension of 1
-            print(f"After unsqueeze shape: {x.shape}")  # Debugging line
-    
-            for encoder in self.encoders:
-                x = encoder(x)
-                print(f"After encoder shape: {x.shape}")  # Debugging line
-    
-            x = x.squeeze(0)  # Remove the sequence length dimension
-            print(f"After squeeze shape: {x.shape}")  # Debugging line
-    
-            x = self.classifier(x)
-            print(f"Output shape: {x.shape}")  # Debugging line
-    
-            return x            
+    def forward(self, x):  # Unindented to match with __init__
+        print(f"Input shape: {x.shape}")  # Debugging line
+        x = self.embedding(x)
+        print(f"After embedding shape: {x.shape}")  # Debugging line
+        x = x.unsqueeze(0)  # Introduce a sequence length dimension of 1
+        print(f"After unsqueeze shape: {x.shape}")  # Debugging line
+        for encoder in self.encoders:
+            x = encoder(x)
+            print(f"After encoder shape: {x.shape}")  # Debugging line
+        x = x.squeeze(0)  # Remove the sequence length dimension
+        print(f"After squeeze shape: {x.shape}")  # Debugging line
+        x = self.classifier(x)
+        print(f"Output shape: {x.shape}")  # Debugging line
+        return x
+
         """
     def forward(self, x):
         print(f"Input shape: {x.shape}")  # Debugging line
