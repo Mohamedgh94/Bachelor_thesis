24b9b7477a58b729dc95a71c040145ba3f7bd26a
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-10-31 01:20:19 +0100

    Update sample_data.py

--- GatedTransformer/sample_data.py
+++ GatedTransformer/sample_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('/data/malghaja/Bachelor_thesis/Sis_train_data.csv')
 sample_df = df.sample(frac=0.01 )
 sample_df.to_csv('sample.csv', index=False)
