71c9cd2d4e6f6f2e2815d9b1b81cf7ccfdbafa91
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-11-08 05:46:03 +0100

    Update saveAndLoad.py

--- GatedTransformer/saveAndLoad.py
+++ GatedTransformer/saveAndLoad.py
@@ -1,4 +1,4 @@
-import torch
+""" import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from sklearn.metrics import confusion_matrix
@@ -104,8 +104,7 @@ class SaveAndLoadModel:
                 all_labels.extend(flat_labels.cpu().numpy())
                 if debug:
                     print("After: ", len(all_outputs), len(all_labels))
-        print('all_labels : ',len(all_labels))  
-        print('all_outputs :', len(all_outputs))
+            
         all_labels = np.array(all_labels).reshape(-1)
         all_outputs = np.array(all_outputs).reshape(-1)
         all_labels = all_labels.astype(int)
@@ -132,16 +131,7 @@ class SaveAndLoadModel:
 
         torch.save(self.model.state_dict(), self.model_path)
         print(f"Model saved to {self.model_path}")
-        """
-    def train_validate_and_test(self, train_loader, valid_loader, test_loader, epochs):
-        self.train_and_validate(train_loader, valid_loader, epochs)
-        metrics = self.test(test_loader)
         
-        # Save metrics to Excel
-        df = pd.DataFrame([metrics], columns=metrics.keys())
-        df.to_excel("results.xlsx", index=False)
-        print("Results saved to results.xlsx")
-        """
     def train_validate_and_test(self, train_loader, valid_loader, test_loader, epochs):
         self.train_and_validate(train_loader, valid_loader, epochs)
         metrics, y_true, y_pred = self.test(test_loader)
@@ -159,7 +149,192 @@ class SaveAndLoadModel:
             plt.xlabel('Predicted Label')
             plt.savefig('confusion_matrix.png')
             plt.close()
+            
             print("Results and confusion matrix saved to results.xlsx")
     def load_model(self):
         self.model.load_state_dict(torch.load(self.model_path))
         print(f"Model loaded from {self.model_path}")
+ """
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score, mean_squared_error, r2_score, mean_absolute_error
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+from loss import MultiTaskLossFunction 
+
+class SaveAndLoadModel:
+    def __init__(self, model, loss_fn, optimizer_class, epochs, model_path="model.pth", device=None):
+        self.model = model
+        self.loss_fn = MultiTaskLossFunction() # This should be an instance of MultiTaskLossFunction
+        self.optimizer = optimizer_class(self.model.parameters())
+        self.epochs = epochs
+        self.model_path = model_path
+        self.device = device or (torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f"Total number of trainable parameters: {total_params}")
+
+    
+   
+    """
+    def train(self, train_loader, epochs=10):
+        multi_task_loss_fn = MultiTaskLossFunction() 
+        trainings_start_time = time.time()
+        #for name, param in self.model.named_parameters():
+        #    print(f"{name} requires_grad: {param.requires_grad}")
+        self.model.train()
+        total_loss = 0
+        for epoch in range(epochs):
+            total_loss = 0
+            correct_predictions = {task: 0 for task in ['person_id', 'gender']}
+            total_predictions = {task: 0 for task in ['person_id', 'gender']}
+            for batch  in train_loader:
+                inputs, labels_dict = batch
+                inputs = inputs.to(self.device)
+                labels_dict = {task: labels.to(self.device) for task, labels in labels_dict.items()}
+                self.optimizer.zero_grad()
+                outputs_dict = self.model(inputs)
+                for task, labels in labels_dict.items():
+                    if task in ['age', 'height', 'weight']:
+
+                        labels_dict[task] = labels.float()
+                    elif task in ['person_id', 'gender']:
+                        labels_dict[task] = labels.long()    
+                loss = multi_task_loss_fn.compute_loss(outputs_dict, labels_dict)
+                loss.backward()
+                self.optimizer.step()
+                total_loss += loss.item()
+            avg_loss = total_loss / len(train_loader)
+            print(f"Epoch {epoch+1}, Avg Loss: {avg_loss}")
+        training_end_time = time.time()
+        print(f'Training completed in {training_end_time - trainings_start_time}s')
+     """
+    
+    def train(self, train_loader, epochs=10):
+        multi_task_loss_fn = MultiTaskLossFunction() 
+        trainings_start_time = time.time()
+        self.model.train()
+    
+        for epoch in range(epochs):
+            total_loss = 0
+            correct_predictions = {task: 0 for task in ['person_id', 'gender']}
+            total_predictions = {task: 0 for task in ['person_id', 'gender']}
+        
+            for batch in train_loader:
+                inputs, labels_dict = batch
+                inputs = inputs.to(self.device)
+                labels_dict = {task: labels.to(self.device) for task, labels in labels_dict.items()}
+            
+                self.optimizer.zero_grad()
+                outputs_dict = self.model(inputs)
+            
+                loss = 0
+                for task, output in outputs_dict.items():
+                    labels = labels_dict[task]
+                    if task in ['age', 'height', 'weight']:
+                        # For regression tasks, you can simply calculate the loss
+                        loss += multi_task_loss_fn.loss_fns[task](output.squeeze(), labels.float())
+                    elif task in ['person_id', 'gender']:
+                        # For classification tasks, calculate loss and accuracy
+                        
+                        loss += multi_task_loss_fn.loss_fns[task](output, labels.long())
+                    
+                    # Calculate the number of correct predictions
+                        _, predicted = torch.max(output, 1)
+                        correct_predictions[task] += (predicted == labels).sum().item()
+                        total_predictions[task] += labels.size(0)
+                    
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+        
+        avg_loss = total_loss / len(train_loader)
+        print(f"Epoch {epoch+1}, Avg Loss: {avg_loss}")
+        
+        # Calculate and print the accuracy for each classification task
+        for task in correct_predictions:
+            accuracy = correct_predictions[task] / total_predictions[task]
+            print(f"Epoch {epoch+1}, {task} Training Accuracy: {accuracy:.4f}")
+        
+        training_end_time = time.time()
+        print(f'Training completed in {training_end_time - trainings_start_time}s')
+
+    def validate(self, valid_loader):
+        multi_task_loss_fn = MultiTaskLossFunction() 
+        self.model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for inputs, labels_dict in valid_loader:
+                inputs = inputs.to(self.device)
+                labels_dict = {task: labels.to(self.device) for task, labels in labels_dict.items()}
+                outputs_dict = self.model(inputs)
+                loss =multi_task_loss_fn.compute_loss(outputs_dict, labels_dict)
+                total_loss += loss.item()
+
+        avg_loss = total_loss / len(valid_loader)
+        print(f"Validation Avg Loss: {avg_loss}")
+        return avg_loss
+
+    def test(self, test_loader):
+        self.model.eval()
+        all_outputs = {}
+        all_labels = {}
+        with torch.no_grad():
+            for inputs, labels_dict in test_loader:
+                inputs = inputs.to(self.device)
+                outputs_dict = self.model(inputs)
+                for task, output in outputs_dict.items():
+                    predicted = output.argmax(dim=1) if task in ['person_id', 'gender'] else output.squeeze()
+                    labels = labels_dict[task]
+                    all_outputs.setdefault(task, []).extend(predicted.cpu().numpy())
+                    all_labels.setdefault(task, []).extend(labels.cpu().numpy())
+
+        # Calculate metrics for each task
+        metrics = {}
+        for task in all_labels:
+            true = np.array(all_labels[task]).reshape(-1)
+            pred = np.array(all_outputs[task]).reshape(-1)
+            if task in ['person_id', 'gender']:
+                accuracy = accuracy_score(true, pred)
+                f1 = f1_score(true, pred, average="weighted")
+                precision = precision_score(true, pred, average="weighted")
+                recall = recall_score(true, pred, average="weighted")
+                cm = confusion_matrix(true, pred)
+                metrics[task] = {"accuracy": accuracy, "f1": f1, "precision": precision, "recall": recall, "confusion_matrix": cm}
+                plt.figure(figsize=(10, 8))
+                sns.heatmap(cm, annot=True, fmt='g', cmap='Blues')
+                plt.title(f"Confusion Matrix for {task}")
+                plt.xlabel('Predicted')
+                plt.ylabel('True')
+                plt.show()
+                
+            else:
+                # Metrics for regression tasks
+                mse = mean_squared_error(true, pred)
+                rmse = mean_squared_error(true, pred, squared=False)
+                mae = mean_absolute_error(true, pred)
+                r2 = r2_score(true, pred)
+                
+                metrics[task] = {"mse": mse, "rmse": rmse, "mae": mae, "r2": r2}
+        
+        print(metrics)
+        return metrics , all_labels, all_outputs
+    def train_and_validate(self, train_loader, valid_loader):
+        self.train(train_loader, self.epochs)
+        self.validate(valid_loader)
+        
+        torch.save(self.model.state_dict(), self.model_path)
+        print(f"Model saved to {self.model_path}")
+
+    def train_validate_and_test(self, train_loader, valid_loader, test_loader):
+        self.train_and_validate(train_loader, valid_loader)
+        self.test(test_loader)
+
+    def load_model(self):
+        self.model.load_state_dict(torch.load(self.model_path))
+        print(f"Model loaded from {self.model_path}")
+
