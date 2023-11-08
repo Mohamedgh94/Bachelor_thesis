24c7da0a7ea993d4c4fefcea8aee5f22377a2107
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-11-08 05:42:53 +0100

    Update IMUDataset.py

--- GatedTransformer/IMUDataset.py
+++ GatedTransformer/IMUDataset.py
@@ -12,6 +12,9 @@ class IMUDataset(Dataset):
         self.labels = self.dataframe.iloc[:, -5:].values
         # Assuming all other columns are features
         self.features = self.dataframe.iloc[:, :-5].values
+        self.label_categories = {}
+        for column in self.dataframe.columns[-5:]:
+            self.label_categories[column] = self.dataframe[column].unique()
         
     def __len__(self):
         return len(self.dataframe)
@@ -19,14 +22,45 @@ class IMUDataset(Dataset):
     def __getitem__(self, idx):
         feature_vector = self.features[idx]
         label_vector = self.labels[idx]
+        label_dict = {
+            'person_id': label_vector[0],
+            'age': label_vector[1],
+            'height': label_vector[2],
+            'weight': label_vector[3],
+            'gender': label_vector[4],
+        }
         
-        return torch.tensor(feature_vector, dtype=torch.float32), torch.tensor(label_vector, dtype=torch.float32)
-
+        return torch.tensor(feature_vector, dtype=torch.float32), label_dict #torch.tensor(label_vector, dtype=torch.float32)
+    @staticmethod
+    def get_combined_categories(*datasets):
+        combined_categories = {}
+        for dataset in datasets:
+            for key, value in dataset.label_categories.items():
+                if key not in combined_categories:
+                    combined_categories[key] = set(value)
+                else:
+                    combined_categories[key].update(value)    
+                #combined_categories[key].update(dataset.label_categories[key])
+            # Convert sets back to lists
+        combined_categories = {key: list(values) for key, values in combined_categories.items()}
+        return combined_categories
 
 # Create instances of the IMUDataset class for each dataset
-train_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/tsample.csv")
-valid_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/vsample.csv")
-test_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/Sis_test_data.csv")
+train_dataset = IMUDataset("/Users/mohamadghajar/Desktop/GatedTransformer/Bachelor_thesis/Unimib_train_data.csv")
+valid_dataset = IMUDataset("/Users/mohamadghajar/Desktop/GatedTransformer/Bachelor_thesis/Unimib_valid_data.csv")
+test_dataset = IMUDataset("/Users/mohamadghajar/Desktop/GatedTransformer/Bachelor_thesis/Unimib_test_data.csv")
+
+combined_categories = IMUDataset.get_combined_categories(train_dataset,valid_dataset,test_dataset)
+global num_person_ids 
+global num_ages
+global num_heights
+global num_weights
+global num_genders
+num_person_ids = len(combined_categories['person_id'])
+num_ages = len (combined_categories['age'])
+num_heights = len(combined_categories['height'])
+num_weights = len(combined_categories['weight'])
+num_genders = len(combined_categories['gender'])
 
 # Create DataLoader instances
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
