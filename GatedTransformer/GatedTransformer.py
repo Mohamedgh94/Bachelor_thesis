85918fa14b7660e38cfb3fb5c60b28e09ebc3e23
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-11-08 05:42:00 +0100

    Update GatedTransformer.py

--- GatedTransformer/GatedTransformer.py
+++ GatedTransformer/GatedTransformer.py
@@ -1,14 +1,21 @@
-import torch
+""" import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from embedding import EmbeddingLayer 
 from encoder import EncoderLayer
+from IMUDataset import IMUDataset
+
 
 
 class GatedTransformer(nn.Module):
-    def __init__(self, input_dim=45, d_model=512, num_heads=8, d_ff=2048, num_layers=6):
+    def __init__(self, input_dim, d_model, num_heads, d_ff, num_layers,num_person_ids , num_ages ,  num_heights, num_weights, num_genders):
         super(GatedTransformer, self).__init__()
+        self.person_ids_classifier = nn.Linear(d_model, num_person_ids)
+        self.age_classifier = nn.Linear(d_model, num_ages)
+        self.height_classifier = nn.Linear(d_model, num_heights)
+        self.weights_classifier = nn.Linear(d_model, num_weights)
+        self.gender_classifier = nn.Linear(d_model,num_genders)
         self.embedding = nn.Linear(input_dim , d_model )
         print(f'self.embedding.weight.shape',self.embedding.weight.shape)
         self.encoders = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff) for _ in range(num_layers)])
@@ -26,30 +33,64 @@ class GatedTransformer(nn.Module):
             #print(f"After encoder shape: {x.shape}")  # Debugging line
         x = x.squeeze(0)  # Remove the sequence length dimension
         # print(f"After squeeze shape: {x.shape}")  # Debugging line
-        x = self.classifier(x)
+        # x = self.classifier(x)
         #print(f"Output shape: {x.shape}")  # Debugging line
-        return x
+        person_id_output = F.log_softmax(self.person_ids_classifier(x), dim=1)
+        age_output = F.log_softmax(self.age_classifier(x), dim=1)
+        height_output = F.log_softmax(self.height_classifier(x), dim=1)
+        weight_output = F.log_softmax(self.weights_classifier(x), dim=1)
+        gender_output = F.log_softmax(self.gender_classifier(x), dim=1)
+        outputs= {
+            'person_id': person_id_output,
+            'age': age_output,
+            'height': height_output,
+            'weight': weight_output,
+            'gender' : gender_output
+        }
+        return outputs
+ """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-        """
+from embedding import EmbeddingLayer 
+from encoder import EncoderLayer
+
+class GatedTransformer(nn.Module):
+    def __init__(self, input_dim, d_model, num_heads, d_ff, num_layers,
+                 num_person_ids, num_ages, num_heights, num_weights, num_genders):
+        super(GatedTransformer, self).__init__()
+        self.person_ids_classifier = nn.Linear(d_model, num_person_ids)
+        self.age_regressor = nn.Linear(d_model, 1)  # Output one value for age
+        self.height_regressor = nn.Linear(d_model, 1)  # Output one value for height
+        self.weight_regressor = nn.Linear(d_model, 1)  # Output one value for weight
+        self.gender_classifier = nn.Linear(d_model, num_genders)
+        self.embedding = nn.Linear(input_dim, d_model)
+        self.encoders = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff) for _ in range(num_layers)])
+
     def forward(self, x):
-        print(f"Input shape: {x.shape}")  # Debugging line
-        
         x = self.embedding(x)
-        print(f"After embedding shape: {x.shape}")  # Debugging line
-        
+        x = x.unsqueeze(0)  # Introduce a sequence length dimension of 1
+
         for encoder in self.encoders:
-            x = x.transpose(0, 1)
             x = encoder(x)
-            x = x.transpose(1, 0)
-            print(f"After encoder shape: {x.shape}")  # Debugging line
-        
-        #x = x.mean(dim=1)
-    
-        #print(f"After mean shape: {x.shape}")  # Debugging line
-        #x = x.view(x.shape[0], -1)
-        #print(f"After view shape: {x.shape}")
-        x = self.classifier(x)
-        print(f"Output shape: {x.shape}")  # Debugging line
-        
-        return x
-        """
+
+        x = x.squeeze(0)  # Remove the sequence length dimension
+
+        # Output raw logits for classification tasks
+        person_id_logits = self.person_ids_classifier(x)
+        gender_logits = self.gender_classifier(x)
+
+        # Output raw values for regression tasks
+        age_output = self.age_regressor(x)
+        height_output = self.height_regressor(x)
+        weight_output = self.weight_regressor(x)
+
+        outputs = {
+            'person_id': person_id_logits,
+            'age': age_output,
+            'height': height_output,
+            'weight': weight_output,
+            'gender': gender_logits
+        }
+        return outputs
