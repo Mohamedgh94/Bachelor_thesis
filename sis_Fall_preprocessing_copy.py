c189923c3654ae9f7f93dfb9888eaf859c6ffcf2
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-12-26 16:40:14 +0100

    modify sis_Fall_preprocessing_copy.py

--- sis_Fall_preprocessing_copy.py
+++ sis_Fall_preprocessing_copy.py
@@ -8,11 +8,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from multiprocessing import Pool
-
-
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
 
 # Configuration Parameters
-DATA_DIR = '/data/malghaja/SisFall_csv' 
+DATA_DIR = '/Users/mohamadghajar/Downloads/SisFallDatasetAnnotation-master/sis_csv' 
 README_FILE_PATH = os.path.join(DATA_DIR, 'Readme.txt')
 SUBJECT_IDS = ['SA01', 'SA02', 'SA03', 'SA04', 'SA05', 'SA06', 'SA07', 'SA08', 'SA09', 'SA10', 'SA11', 'SA12', 'SA13', 'SA14', 'SA15', 'SA16', 'SA17', 'SA18', 'SA19', 'SA20', 'SA21', 'SA22', 'SA23', 'SE01', 'SE02']
 WINDOW_SIZE = 200
@@ -21,7 +22,7 @@ SOFT_BIOMETRICS = ['age', 'height', 'weight', 'gender']
 
 def get_person_info(subject_id):
     # Open the readme file and read the lines
-    file_path = '/data/malghaja/SisFall_csv/Readme.txt'
+    file_path = '/Users/mohamadghajar/Downloads/SisFallDatasetAnnotation-master/sis_csv/Readme.txt'
     with open(file_path, 'r', encoding='latin1') as file:
         strings = file.readlines()
 
@@ -94,7 +95,7 @@ def normalize_and_encode(all_data):
         # Encode the person IDs and soft biometric labels
         print('Encoding labels...')
         label_encoders = {}  
-        for col in ['person_id'] + SOFT_BIOMETRICS:
+        for col in ['person_id','gender'] :
             le = LabelEncoder()
             all_data[col] = le.fit_transform(all_data[col])
             label_encoders[col] = le  # Store the encoder
@@ -130,7 +131,52 @@ def rearrange_columns(df):
     return df[cols]
 
 def split_and_save_data(X, y):
+   
     try:
+        # Concatenate the labels into a single string for stratification
+        # y_stratify = y.apply(lambda x: '_'.join(x.map(str)), axis=1)
+        print('Splitting data...')
+        print (X.columns, y.columns)
+        unique_person_ids = y['person_id'].unique()
+        # Split the data into training and validation sets
+        train_ids, temp_ids = train_test_split(unique_person_ids, test_size=0.3, random_state=42)
+        valid_ids, test_ids = train_test_split(temp_ids, test_size=0.5, random_state=42)
+
+        # Filter y based on the train, validation, and test ids and get indices for X
+        train_indices = y.index[y['person_id'].isin(train_ids)].tolist()
+        valid_indices = y.index[y['person_id'].isin(valid_ids)].tolist()
+        test_indices = y.index[y['person_id'].isin(test_ids)].tolist()
+
+        # Use the indices to filter X
+        X_train = X.iloc[train_indices]
+        y_train = y.iloc[train_indices]
+        X_valid = X.iloc[valid_indices]
+        y_valid = y.iloc[valid_indices]
+        X_test = X.iloc[test_indices]
+        y_test = y.iloc[test_indices]
+
+        over = SMOTE(sampling_strategy=0.5)  # Adjust as needed
+        under = RandomUnderSampler(sampling_strategy=0.8)  # Adjust as needed
+        steps = [('o', over), ('u', under)]
+        pipeline = Pipeline(steps=steps)
+
+        # Apply the pipeline to your training data
+        X_train_resampled, y_train_resampled = pipeline.fit_resample(X_train, y_train['person_id'])
+        
+
+        print('Splitting complete.')
+
+        # Concatenate the features and labels for each dataset
+        #train_data = pd.concat([X_train, y_train], axis=1)
+        train_data = pd.concat([X_train_resampled, y_train_resampled], axis=1)
+        train_class_counts = train_data['person_id'].value_counts()
+        print(f"Train class counts:\n{train_class_counts}")
+        valid_data = pd.concat([X_valid, y_valid], axis=1)
+        test_data = pd.concat([X_test, y_test], axis=1)
+        train_data = train_data.sample(frac=1,random_state=1).reset_index(drop=True)
+        valid_data = valid_data.sample(frac=1,random_state=1).reset_index(drop=True)
+        test_data = test_data.sample(frac=1,random_state=1).reset_index(drop=True)
+        """  try:
         # Concatenate the labels into a single string for stratification
         # y_stratify = y.apply(lambda x: '_'.join(x.map(str)), axis=1)
         print('Splitting data...')
@@ -148,8 +194,9 @@ def split_and_save_data(X, y):
         train_data.to_csv('Sis_train_data.csv', index=False)
         valid_data.to_csv('Sis_valid_data.csv', index=False)
         test_data.to_csv('Sis_test_data.csv', index=False)
+        """
     except Exception as e:
-        print(f"Error in split_and_save_data: {e}")
+        print(f"Error in split_and_save_data: {e}") 
 
 
 def main():
