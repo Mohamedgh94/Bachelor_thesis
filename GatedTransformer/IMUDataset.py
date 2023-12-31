34cd7f480694b3b1d55639d6b62e9cf4b251fc05
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2023-12-31 15:50:55 +0100

    --

--- GatedTransformer/IMUDataset.py
+++ GatedTransformer/IMUDataset.py
@@ -53,9 +53,9 @@ class IMUDataset(Dataset):
             weights[class_idx] = total_samples / (num_classes * count)
         return weights
 # Create instances of the IMUDataset class for each dataset
-train_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/SisCat_train_data.csv")
-valid_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/SisCat_valid_data.csv")
-test_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/SisCat_test_data.csv")
+train_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/UniCat_train_data.csv ")
+valid_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/UniCat_valid_data.csv")
+test_dataset = IMUDataset("/data/malghaja/Bachelor_thesis/UniCat_test_data.csv")
 # train_dataset = IMUDataset("/Users/mohamadghajar/Documents/BAC/SisCat_train_data.csv")
 # valid_dataset = IMUDataset("/Users/mohamadghajar/Documents/BAC/SisCat_valid_data.csv")
 # test_dataset= IMUDataset("/Users/mohamadghajar/Documents/BAC/SisCat_test_data.csv")
