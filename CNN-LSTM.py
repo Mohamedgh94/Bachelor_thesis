28478fa76543158a17a8df551b72cac861d7db71
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-01-29 16:41:48 +0100

    edit log file

--- CNN-LSTM.py
+++ CNN-LSTM.py
@@ -573,11 +573,12 @@ def uniMib_main():
     """
 
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           usage_mod_idx= 1 , learning_rates_idx=2,batch_size_idx=2 ,input_size_idx= 0,
+                           usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 0,
                             epochs=15)
     #print(config)
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     log_filename = f"{config['folder_exp']}logger_{timestamp}.txt"
+
     setup_experiment_logger(logging_level=logging.DEBUG, filename=log_filename)
     #setup_experiment_logger(logging_level=logging.DEBUG, filename=config['folder_exp'] + "logger.txt")
     logging.info('Finished UniMib experiment setup')
