2bca46034476459588dc6bbb8059ad69afbed466
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-01-31 17:12:09 +0100

    -

--- Network.py
+++ Network.py
@@ -567,7 +567,7 @@ def uniMib_main():
 
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=1, 
                            usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 0,
-                            gpudevice_idx=2,epochs=15)
+                            gpudevice_idx=2,epochs=15) 
     #print(config)
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     log_filename = f"{config['folder_exp']}logger_{timestamp}.txt"
