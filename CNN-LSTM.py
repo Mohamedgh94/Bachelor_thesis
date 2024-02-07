4beb18623929645c2336e2b8c95035a2e9e2e3ba
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-07 09:18:33 +0100

    -

--- CNN-LSTM.py
+++ CNN-LSTM.py
@@ -683,8 +683,8 @@ def sisFall_main():
     Run experiment for SisFall dataset with predefined parameters.
     """
 
-    config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=1, 
-                           usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=1 ,input_size_idx= 1,
+    config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=0, 
+                           usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=1 ,input_size_idx= 1,
                             gpudevice_idx= 1,epochs=15)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -719,7 +719,7 @@ def mobiact_main():
 if __name__ == "__main__":
 
     #main()
-    uniMib_main()
+    #uniMib_main()
 
-    #sisFall_main()
+    sisFall_main()
     #mobiact_main()
