072eedbc914959d7affb0a4040dc9650e2e6ed1b
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-11 08:18:21 +0100

    -

--- CNN-LSTM_copy.py
+++ CNN-LSTM_copy.py
@@ -671,7 +671,7 @@ def uniMib_main():
     """
 
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           gpudevice_idx=1,usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 0,
+                           gpudevice_idx=2,usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 0,
                             epochs=15)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
