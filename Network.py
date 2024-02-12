e86f16f7fb2ef0db8787191463a9b3750383e3f8
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-12 02:23:59 +0100

    -

--- Network.py
+++ Network.py
@@ -717,7 +717,7 @@ def uniMib_main():
     """
 
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           gpudevice_idx=1,usage_mod_idx= 2 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 0,
+                           gpudevice_idx=1,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 0,
                             epochs=15)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -744,7 +744,7 @@ def sisFall_main():
 
     config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=0, 
                            usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=1 ,input_size_idx= 1,
-                            gpudevice_idx= 0,epochs=15)
+                            gpudevice_idx= 2,epochs=15)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     #log_filename = f"{config['folder_exp']}logger_{timestamp}.txt"
