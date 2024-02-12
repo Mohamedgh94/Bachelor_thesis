b3caaa20d9974786d9b9f87bde0e7467cea95393
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-12 09:18:07 +0100

    -

--- Network.py
+++ Network.py
@@ -46,8 +46,7 @@ class IMUDataset(Dataset):
             'gender': torch.tensor(label_vector[4], dtype=torch.long),
         }
         # Reshape the feature vector into a 2D matrix (1x4x6 for a single channel)
-        feature_vector = feature_vector.reshape(1, 4, 6)  # Adjust the reshape dimensions as necessary
-
+        feature_vector = feature_vector.reshape(1, 4, 6)  
         if self.transform:
             feature_vector = self.transform(feature_vector)
 
@@ -716,8 +715,8 @@ def uniMib_main():
     Run experiment for UniMib dataset with predefined parameters.
     """
 
-    config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           gpudevice_idx=1,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 0,
+    config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=1, 
+                           gpudevice_idx=2,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 0,
                             epochs=15)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
