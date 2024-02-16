f87165db1aa51f2c43dbd35259bad25f5a84c7b6
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-16 19:03:34 +0100

    -

--- CNN-LSTM_copy.py
+++ CNN-LSTM_copy.py
@@ -46,8 +46,8 @@ class IMUDataset(Dataset):
             'gender': torch.tensor(label_vector[4], dtype=torch.long),
         }
         # Reshape the feature vector into a 2D matrix (1x4x6 for a single channel)
-        #feature_vector = feature_vector.reshape(1, 5, 9) 
-        feature_vector = feature_vector.reshape(1, 6, 4)  
+        feature_vector = feature_vector.reshape(1, 5, 9) 
+        #feature_vector = feature_vector.reshape(1, 6, 4)  
         if self.transform:
             feature_vector = self.transform(feature_vector)
 
@@ -576,10 +576,10 @@ def run_network(configuration,logger):
             print(f'Epoch {epoch+1} trainng time {trainng_time}')
             logger.info(f"Epoch {epoch+1}, Training Loss: {train_loss}, Validation Loss: {val_loss}")
 
-            early_stopping(val_loss)
-            if early_stopping.early_stop:
-                print("Early stopping triggered")
-                break
+            #early_stopping(val_loss)
+            #if early_stopping.early_stop:
+            #    print("Early stopping triggered")
+            #    break
 
         plot_learning_curve(train_losses, val_losses)
         model_save_path = f"CNN-LSTM_{configuration['dataset']}_lr{configuration['learning_rate']}_bs{configuration['batch_size']}_model.pth"
@@ -662,7 +662,7 @@ def sisFall_main():
 def mobiact_main():
     
     config = configuration(dataset_idx=2, dataset_paths = 'MobiAct',output_idx=1, 
-                           usage_mod_idx= 1 , learning_rates_idx=2,batch_size_idx=2 ,input_size_idx= 1,
+                           usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 1,
                             gpudevice_idx= 1,epochs=10)
      
     experiment_logger, log_filename = setup_experiment_logger(experiment_name='Mobiact_ident')   
