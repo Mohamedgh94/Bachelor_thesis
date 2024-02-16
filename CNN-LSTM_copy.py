490566d4e79baac5c1c4a4b1677a8c26285268b1
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-16 19:06:54 +0100

    --

--- CNN-LSTM_copy.py
+++ CNN-LSTM_copy.py
@@ -639,8 +639,8 @@ def sisFall_main():
     """
 
     config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=1, 
-                           usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=1 ,input_size_idx= 1,
-                            gpudevice_idx= 0,epochs=10)
+                           usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=2 ,input_size_idx= 1,
+                            gpudevice_idx= 2,epochs=10)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     #log_filename = f"{config['folder_exp']}logger_{timestamp}.txt"
@@ -650,7 +650,7 @@ def sisFall_main():
     # if not os.path.exists(dir_name):
     #     os.makedirs(dir_name)
 
-    experiment_logger, log_filename = setup_experiment_logger(experiment_name='SisFall_identification')   
+    experiment_logger, log_filename = setup_experiment_logger(experiment_name='SisFall_identification_Ex')   
     experiment_logger.info('Finished UniMib experiment setup')
     # setup_experiment_logger(logging_level=logging.DEBUG, filename=log_filename)
     # #setup_experiment_logger(logging_level=logging.DEBUG, filename=config['folder_exp'] + "logger.txt")
@@ -676,5 +676,5 @@ if __name__ == "__main__":
     #main()
     #uniMib_main()
 
-    #sisFall_main()
-    mobiact_main()
+    sisFall_main()
+   # mobiact_main()
