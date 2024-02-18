e15e3bf580a6b195b9a63b1f22249a66fd201a19
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-18 23:04:29 +0100

    -

--- GatedTransformer/main.py
+++ GatedTransformer/main.py
@@ -258,7 +258,7 @@ class Main:
 def uniMib_main():
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=1, 
                         gpudevice_idx=0,usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=1 ,input_size_idx= 0,
-                            epochs=5)
+                            epochs=1)
     experiment_logger, log_filename  = setup_experiment_logger(experiment_name='GTN_Unimib_identification')    
     experiment_logger.info('Finished UniMib experiment setup')
     model = GatedTransformer(input_dim=config["input_size"],d_model=512, num_heads=16, d_ff=1024, num_layers=6,config = config,num_classes=config['num_classes'] ,dropout_rate=0.1)
