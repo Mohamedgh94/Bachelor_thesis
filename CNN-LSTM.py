07f63870560c3efd61c80ebbc0fd4a8607f921f1
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-27 17:12:47 +0100

    -

--- CNN-LSTM.py
+++ CNN-LSTM.py
@@ -716,7 +716,7 @@ def uniMib_main():
     """
 
     config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           gpudevice_idx=0,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=1 ,input_size_idx= 0,
+                           gpudevice_idx=0,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=0 ,input_size_idx= 0,
                             epochs=5)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -742,7 +742,7 @@ def sisFall_main():
     """
 
     config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=0, 
-                           usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 1,
+                           usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=0 ,input_size_idx= 1,
                             gpudevice_idx= 1,epochs=5)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -779,5 +779,5 @@ if __name__ == "__main__":
     #main()
     #uniMib_main()
 
-    #sisFall_main()
-    mobiact_main()
+    sisFall_main()
+    #mobiact_main()
