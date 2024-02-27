666cc5e4793a2a677af0dcfdc0c5db69432b0919
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-27 07:15:15 +0100

    -

--- CNN-LSTM.py
+++ CNN-LSTM.py
@@ -742,7 +742,7 @@ def sisFall_main():
     """
 
     config = configuration(dataset_idx=1, dataset_paths = 'SisFall',output_idx=0, 
-                           usage_mod_idx= 2 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 1,
+                           usage_mod_idx= 1 , learning_rates_idx=0,batch_size_idx=2 ,input_size_idx= 1,
                             gpudevice_idx= 1,epochs=5)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
