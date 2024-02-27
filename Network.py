33398e3ef688e7c391c35d10b9d5b753f2ef122a
Mohamedgh94 <95039625+Mohamedgh94@users.noreply.github.com>
2024-02-27 17:16:03 +0100

    -

--- Network.py
+++ Network.py
@@ -715,8 +715,8 @@ def uniMib_main():
     Run experiment for UniMib dataset with predefined parameters.
     """
 
-    config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=0, 
-                           gpudevice_idx=0,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=0 ,input_size_idx= 0,
+    config = configuration(dataset_idx=0, dataset_paths = 'Unimib',output_idx=1, 
+                           gpudevice_idx=2,usage_mod_idx= 1 , learning_rates_idx=1,batch_size_idx=0 ,input_size_idx= 0,
                             epochs=5)
     #print(config)
     #timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
